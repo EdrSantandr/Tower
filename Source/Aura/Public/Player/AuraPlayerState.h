ffd62b709126fc6108f4d4126a74da9a587c2729
Eder Santander <eder@bamtang.com>
2023-12-15 09:25:58 -0500

    Added Delegates for XP and Level to player state

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -10,6 +10,8 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/);
+
 /**
  * 
  */
@@ -29,6 +31,20 @@ public:
 
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
 
+	FORCEINLINE int32 GetPlayerXP() const { return XP; }
+
+	FOnPlayerStatChanged OnXPChangedDelegate;
+
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+
+	void AddtoLevel(int32 InLevel);
+
+	void SetLevel(int32 InLevel);
+
+	void AddtoXP(int32 InXP);
+
+	void SetXP(int32 InXP);
+
 protected:
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
@@ -43,4 +59,10 @@ private:
 
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UPROPERTY(VisibleAnywhere, ReplicatedUsing=OnRep_XP)
+	int32 XP = 0;
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
 };
