fd4d4974812f7133f3fbd226adcea8232541ae49
Eder Santander <eder@bamtang.com>
2023-12-15 09:08:01 -0500

    Added Levelupinfo to handle reward points and experience required

---
+++ Source/Aura/Private/AbilitySystem/Data/LevelUpInfo.cpp
@@ -0,0 +1,39 @@
+// Copyright Eder
+
+
+#include "AbilitySystem/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+ 	int32 Level = 0;
+	for (FAuraLevelUpInfo LevelupInfo :LevelUpInformation)
+	{
+		if ( XP >= LevelupInfo.LevelUpRequirement)
+		{
+			Level++;
+		}
+	}	
+	return Level;
+	
+	// Alternate DRUID CODE for level
+	/*INIT
+	
+	int32 Level = 1;
+	bool bSearching =true;
+	while (bSearching)
+	{
+		if (LevelUpInformation.Num() - 1 <= Level) return Level;
+
+		if (XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching=false;
+		}
+	}
+	return Level;
+
+	END*/
+}
