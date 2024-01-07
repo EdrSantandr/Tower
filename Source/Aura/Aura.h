6c09c08f30da473dbd1d928548d2a04db0c477c7
Eder Santander <eder@bamtang.com>
2024-01-07 15:59:24 -0500

    Fix minor issues to FadeActor and collision channels

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -7,3 +7,5 @@
 #define CUSTOM_DEPTH_RED 250
 //It's channel 1 because it's the first we created
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
+//It's channel 2 because it's the second we created
+#define ECC_Target ECollisionChannel::ECC_GameTraceChannel2
